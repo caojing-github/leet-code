8122f16944b7eb69d864b3af0a0990816f1498c1
caojing <caojing0229@foxmail.com>
2019-11-18 16:23:47 +0800

    备份

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ bytedance目录为字节跳动板块的算法题
 [Java中的<< 和 >> 和 >>> 详细分析](https://www.cnblogs.com/chuijingjing/p/9405598.html)  
 [ASCII 、UTF-8、Unicode都是个啥啊，为啥会乱码啊？](https://www.cnblogs.com/angelye/p/8249711.html)  
 [UTF-8编码规则](https://www.cnblogs.com/xinruzhishui/p/5763894.html)  
-
+[PriorityQueue详解](https://www.jianshu.com/p/f1fd9b82cb72)  
 
--- src/bytedance/Solution12.java
+++ src/bytedance/Solution12.java
@@ -1,5 +1,7 @@
 package bytedance;
 
+import java.util.PriorityQueue;
+
 /**
  * 数组中的第K个最大元素  https://leetcode-cn.com/explore/interview/card/bytedance/243/array-and-sorting/1018/    TODO
  *
@@ -53,7 +55,27 @@ public class Solution12 {
         }
     }
 
+    /**
+     * 方法一：堆 官方题解：https://leetcode-cn.com/problems/kth-largest-element-in-an-array/solution/shu-zu-zhong-de-di-kge-zui-da-yuan-su-by-leetcode/
+     */
+    public static int findKthLargest2(int[] nums, int k) {
+        // init heap 'the smallest element first'
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+
+        // keep k largest elements in the heap
+        for (int n : nums) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        // output
+        return heap.poll();
+    }
+
     public static void main(String[] args) {
         System.out.println(findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+        System.out.println(findKthLargest2(new int[]{3, 2, 1, 5, 6, 4}, 2));
     }
 }
