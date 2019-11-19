ff5f9212c6d0c3d7f7a7fd4be6cdd0ce1b1d060a
caojing <caojing0229@foxmail.com>
2019-11-19 15:53:31 +0800

    备份

--- src/bytedance/Solution20.java
+++ src/bytedance/Solution20.java
@@ -1,7 +1,7 @@
 package bytedance;
 
 /**
- * 两数相加 https://leetcode-cn.com/explore/interview/card/bytedance/244/linked-list-and-tree/1022/ TODO
+ * 两数相加 https://leetcode-cn.com/explore/interview/card/bytedance/244/linked-list-and-tree/1022/
  *
  * @author CaoJing
  * @date 2019/11/12 09:08
@@ -19,45 +19,45 @@ public class Solution20 {
 
     /**
      * 用时最快 https://leetcode-cn.com/submissions/detail/36254231/
+     * 官方题解 https://leetcode-cn.com/problems/add-two-numbers/solution/liang-shu-xiang-jia-by-leetcode/
      */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int c = 0;
-        ListNode pre = null;
-        ListNode res = l1;
-        while (l1 != null || l2 != null) {
-            if (l1 == null) {
-                pre.next = l2;
-                l1 = l2;
-                l2 = null;
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            // 进位
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) {
+                p = p.next;
             }
-            l1.val += (l2 == null) ? c : (l2.val + c);
-            c = 0;
-            if (l1.val > 9) {
-                l1.val %= 10;
-                c = 1;
+            if (q != null) {
+                q = q.next;
             }
-            pre = l1;
-            l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
         }
-        if (c != 0) {
-            ListNode hi = new ListNode(1);
-            pre.next = hi;
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
         }
-        return res;
+        return dummyHead.next;
     }
 
     public static void main(String[] args) {
-        // 链表2 -> 4 -> 3
+        // 链表2 -> 4 -> 3 表示342
         ListNode l1 = new ListNode(2);
         l1.next = new ListNode(4);
         l1.next.next = new ListNode(3);
 
-        // 链表5 -> 6 -> 4
+        // 链表5 -> 6 -> 4 表示 465
         ListNode l2 = new ListNode(5);
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(4);
 
+        // 342 + 465 = 807
         ListNode l3 = addTwoNumbers(l1, l2);
         System.out.println(l3);
     }
