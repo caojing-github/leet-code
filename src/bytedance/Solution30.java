620a96ed3baa4f719df7237e45e1053259104262
caojing <caojing0229@foxmail.com>
2019-11-22 09:57:36 +0800

    备份

--- src/bytedance/Solution30.java
+++ src/bytedance/Solution30.java
@@ -1,7 +1,7 @@
 package bytedance;
 
 /**
- * 最大子序和 https://leetcode-cn.com/explore/interview/card/bytedance/246/dynamic-programming-or-greedy/1029/   TODO
+ * 最大子序和 https://leetcode-cn.com/explore/interview/card/bytedance/246/dynamic-programming-or-greedy/1029/
  *
  * @author CaoJing
  * @date 2019/11/12 10:07
@@ -9,22 +9,23 @@ package bytedance;
 public class Solution30 {
 
     /**
-     * 用时最快 https://leetcode-cn.com/submissions/detail/36262090/
+     * 题解 https://leetcode-cn.com/problems/maximum-subarray/solution/hua-jie-suan-fa-53-zui-da-zi-xu-he-by-guanpengchn/
      */
     public static int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
-        int curSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            curSum += nums[i];
-            if (curSum > maxSum) {
-                maxSum = curSum;
+        int ans = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
             }
-            if (curSum < 0) {
-                curSum = 0;
-            }
+            ans = Math.max(ans, sum);
         }
-        return maxSum;
+        return ans;
     }
+
+    // 用时最快 https://leetcode-cn.com/submissions/detail/36262090/
 
     public static void main(String[] args) {
         System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
