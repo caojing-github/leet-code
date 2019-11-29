9c00d69e591e0f4fae14fa0b918ebcf2edae473a
caojing <caojing0229@foxmail.com>
2019-11-29 10:45:43 +0800

    备份

--- src/bytedance/Solution7.java
+++ src/bytedance/Solution7.java
@@ -1,63 +1,74 @@
 package bytedance;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 复原IP地址 https://leetcode-cn.com/explore/interview/card/bytedance/242/string/1044/     TODO
+ * 复原IP地址 https://leetcode-cn.com/explore/interview/card/bytedance/242/string/1044/
  *
  * @author CaoJing
  * @date 2019/11/11 22:55
  */
 public class Solution7 {
 
-    public String s;
-    public int[] path;
-    List<String> ans;
+    int n;
+    String s;
+    LinkedList<String> segments = new LinkedList<>();
+    ArrayList<String> output = new ArrayList<>();
 
     /**
-     * 用时最快 https://leetcode-cn.com/submissions/detail/36240326/
+     * 回溯(DFS) https://leetcode-cn.com/problems/restore-ip-addresses/solution/fu-yuan-ipdi-zhi-by-leetcode/
+     * 提交答案详情：https://leetcode-cn.com/submissions/detail/36240326/
      */
-    public List<String> restoreIpAddresses(String original) {
-        ans = new ArrayList<>();
-        s = original;
-        path = new int[5];
-        path[4] = s.length();
-        dfs(0, 0);
-        return ans;
+    public List<String> restoreIpAddresses(String s) {
+        n = s.length();
+        this.s = s;
+        backtrack(-1, 3);
+        return output;
     }
 
-    public void dfs(int beg, int cnt) {
-        if (cnt == 4) {
-            if (beg != s.length())
-                return;
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 4; i++) {
-                sb.append(s.substring(path[i], path[i + 1]));
-                if (i != 3)
-                    sb.append(".");
-            }
-            ans.add(sb.toString());
-            return;
-        }
-        if ((s.length() - beg) > 3 * (4 - cnt) || (s.length() - beg) < 1 * (4 - cnt))
-            return;
-        if (s.charAt(beg) != '0') {
-            path[cnt] = beg;
-            dfs(beg + 1, cnt + 1);
-            if (beg + 1 < s.length()) {
-                dfs(beg + 2, cnt + 1);
-            }
-            if (beg + 2 < s.length()) {
-                if (((s.charAt(beg) - '0') * 100 + (s.charAt(beg + 1) - '0') * 10 + s.charAt(beg + 2) - '0') < 256)
-                    dfs(beg + 3, cnt + 1);
-            }
-        } else {
-            path[cnt] = beg;
-            dfs(beg + 1, cnt + 1);
-        }
+    /**
+     * @param prevPos 上一个放置的点
+     * @param dots    待放置点的数量
+     */
+    public void backtrack(int prevPos, int dots) {
+        int maxPos = Math.min(n - 1, prevPos + 4);
+        // 遍历三个有效位置curr_pos以放置点
+        for (int currPos = prevPos + 1; currPos < maxPos; currPos++) {
+            String segment = s.substring(prevPos + 1, currPos + 1);
 
-        return;
+//            System.out.println(String.format("上一指针:%s,对应元素:%s,当前指针:%s对应元素:%s,中间的元素:%s",
+//                prevPos, prevPos == -1 ? "无" : s.charAt(prevPos), currPos, currPos == -1 ? "无" : s.charAt(currPos), segment));
+
+            if (valid(segment)) {
+                segments.add(segment);
+                if (dots - 1 == 0) {
+                    updateOutput(currPos);
+                } else {
+                    backtrack(currPos, dots - 1);
+                }
+                segments.removeLast();
+            }
+        }
+    }
+
+    public boolean valid(String segment) {
+        int m = segment.length();
+        if (m > 3) {
+            return false;
+        }
+        return (segment.charAt(0) != '0') ? (Integer.valueOf(segment) <= 255) : (m == 1);
+    }
+
+    public void updateOutput(int currPos) {
+        String segment = s.substring(currPos + 1, n);
+        System.out.println(String.join(".", segments) + "." + segment);
+        if (valid(segment)) {
+            segments.add(segment);
+            output.add(String.join(".", segments));
+            segments.removeLast();
+        }
     }
 
     public static void main(String[] args) {
--- src/bytedance/Solution9.java
+++ src/bytedance/Solution9.java
@@ -1,7 +1,7 @@
 package bytedance;
 
 /**
- * 岛屿的最大面积 https://leetcode-cn.com/explore/interview/card/bytedance/243/array-and-sorting/1034/ TODO
+ * 岛屿的最大面积 https://leetcode-cn.com/explore/interview/card/bytedance/243/array-and-sorting/1034/
  *
  * @author CaoJing
  * @date 2019/11/11 23:11
@@ -10,6 +10,7 @@ public class Solution9 {
 
     /**
      * 用时最快 https://leetcode-cn.com/submissions/detail/36241633/
+     * 解题思路 https://leetcode-cn.com/problems/max-area-of-island/solution/dao-yu-de-zui-da-mian-ji-by-leetcode/
      */
     public int maxAreaOfIsland(int[][] grid) {
         int maxArea = 0;
@@ -35,10 +36,17 @@ public class Solution9 {
         }
 
         if (grid[row][weight] == 1) {
-            grid[row][weight] = 0; // 标记为以搜索过
-            return 1 + getAreaOfIsLand(grid, row - 1, weight) + getAreaOfIsLand(grid, row + 1, weight) +
-                getAreaOfIsLand(grid, row, weight - 1) + getAreaOfIsLand(grid, row, weight + 1);
-
+            // 标记为以搜索过
+            grid[row][weight] = 0;
+            return 1
+                // 向上
+                + getAreaOfIsLand(grid, row - 1, weight)
+                // 向右
+                + getAreaOfIsLand(grid, row + 1, weight)
+                // 向左
+                + getAreaOfIsLand(grid, row, weight - 1)
+                // 向下
+                + getAreaOfIsLand(grid, row, weight + 1);
         }
         return 0;
     }
