904544b8a3efed5eac41943359824ef7a6c57122
caojing <caojing0229@foxmail.com>
2019-12-01 18:46:49 +0800

    备份

--- src/bytedance/Solution32.java
+++ src/bytedance/Solution32.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 /**
  * **难**
  * <p>
- * 俄罗斯套娃信封问题 https://leetcode-cn.com/explore/interview/card/bytedance/246/dynamic-programming-or-greedy/1031/   TODO
+ * 俄罗斯套娃信封问题 https://leetcode-cn.com/explore/interview/card/bytedance/246/dynamic-programming-or-greedy/1031/
  *
  * @author CaoJing
  * @date 2019/11/12 10:11
@@ -15,51 +15,46 @@ public class Solution32 {
 
     /**
      * 用时最快 https://leetcode-cn.com/submissions/detail/36262367/
+     * 题解 https://www.jiuzhang.com/solution/russian-doll-envelopes
      */
     public int maxEnvelopes(int[][] envelopes) {
-        int len = envelopes.length;
-        if (len < 2) {
-            return len;
+        if (envelopes == null || envelopes.length == 0
+            || envelopes[0] == null || envelopes[0].length != 2) {
+            return 0;
         }
-
         Arrays.sort(envelopes, new Comparator<int[]>() {
             @Override
-            public int compare(int[] envelope1, int[] envelope2) {
-                if (envelope1[0] != envelope2[0]) {
-                    return envelope1[0] - envelope2[0];
+            public int compare(int[] arr1, int[] arr2) {
+                if (arr1[0] == arr2[0]) {
+                    return arr2[1] - arr1[1];
+                } else {
+                    return arr1[0] - arr2[0];
                 }
-                return envelope2[1] - envelope1[1];
             }
         });
-
-
-        int[] tail = new int[len];
-        tail[0] = envelopes[0][1];
-
-        // end 表示有序数组 tail 的最后一个已经赋值元素的索引
-        int end = 0;
-
-        for (int i = 1; i < len; i++) {
-            int target = envelopes[i][1];
-
-            if (target > tail[end]) {
-                end++;
-                tail[end] = target;
-            } else {
-                int left = 0;
-                int right = end;
-
-                while (left < right) {
-                    int mid = (left + right) >> 1;
-                    if (tail[mid] < target) {
-                        left = mid + 1;
-                    } else {
-                        right = mid;
-                    }
-                }
-                tail[left] = target;
+        int[] dp = new int[envelopes.length];
+        int len = 0;
+        for (int[] envelope : envelopes) {
+            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
+            if (index < 0) {
+                index = -index - 1;
+            }
+            dp[index] = envelope[1];
+            if (index == len) {
+                len++;
             }
         }
-        return end + 1;
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[][] envelopes = {
+            {5, 4},
+            {6, 5},
+            {6, 7},
+            {2, 3}
+        };
+        Solution32 solution32 = new Solution32();
+        System.out.println(solution32.maxEnvelopes(envelopes));
     }
 }
