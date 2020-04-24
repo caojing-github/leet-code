43568a47a6eb9df8575fdd531caca13f15a18e68
caojing <caojing0229@foxmail.com>
2020-04-25 07:54:15 +0800

    备份

---
+++ src/algorithm/Solution300A.java
@@ -0,0 +1,39 @@
+package algorithm;
+
+import java.util.Arrays;
+
+/**
+ * 最长上升子序列 https://leetcode-cn.com/problems/longest-increasing-subsequence/
+ *
+ * @author CaoJing
+ * @date 2020/04/12 17:29
+ */
+public class Solution300A {
+
+    /**
+     * https://leetcode-cn.com/problems/longest-increasing-subsequence/solution/dong-tai-gui-hua-er-fen-cha-zhao-tan-xin-suan-fa-p/
+     */
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return len;
+        }
+
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
